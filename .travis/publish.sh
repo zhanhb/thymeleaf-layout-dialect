aa6e01fe80d35a97ce7eff27b8a56556f4196fb7
zhanhb <zhanhb88@gmail.com>
2016-07-21 15:40:29 +0800

    Merge configuration from master

---
+++ .travis/publish.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if git config --get remote.origin.url 2>&1 | grep -F https://github.com/zhanhb/thymeleaf-layout-dialect -q && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    mvn -Duser.name=zhanhb -DskipTests=true clean source:jar javadoc:jar deploy -s .travis/settings.xml $*
+    exit $?
+fi
+
