2833811cd1a052f0db7c5670990edee00f206e50
zhanhb <6323014+zhanhb@users.noreply.github.com>
2019-04-06 17:53:21 +0800

    mvnw instead of mvn

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if git config --get remote.origin.url 2>&1 | grep -F https://github.com/zhanhb/thymeleaf-layout-dialect -q && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-    mvn -Duser.name=zhanhb -DskipTests=true clean deploy -s .travis/settings.xml $*
+    ./mvnw -Duser.name=zhanhb -DskipTests=true clean deploy -s .travis/settings.xml $*
     if [[ $? -ne 0 ]];then exit $?;fi
 fi
 
