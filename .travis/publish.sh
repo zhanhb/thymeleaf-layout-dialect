42f4b1d18c34742566e2c1605d788c4f76ea753b
zhanhb <6323014+zhanhb@users.noreply.github.com>
2019-04-06 17:54:35 +0800

    Manifest Entry Built By

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if git config --get remote.origin.url 2>&1 | grep -F https://github.com/zhanhb/thymeleaf-layout-dialect -q && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-    ./mvnw -Duser.name=zhanhb -DskipTests=true clean deploy -s .travis/settings.xml $*
+    ./mvnw -DskipTests=true clean deploy -s .travis/settings.xml $*
     if [[ $? -ne 0 ]];then exit $?;fi
 fi
 
