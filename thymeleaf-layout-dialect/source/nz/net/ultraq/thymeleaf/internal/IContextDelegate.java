b20523dbbcef4b66360d2bc4596abff47f149803
zhanhb <6323014+zhanhb@users.noreply.github.com>
2020-11-04 19:41:35 +0800

    Fix lookup
    use lookup instead of publicLookup

--- thymeleaf-layout-dialect/source/nz/net/ultraq/thymeleaf/internal/IContextDelegate.java
+++ thymeleaf-layout-dialect/source/nz/net/ultraq/thymeleaf/internal/IContextDelegate.java
@@ -1,8 +1,8 @@
 package nz.net.ultraq.thymeleaf.internal;
 
 import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodHandles;
 import static java.lang.invoke.MethodHandles.*;
+import java.lang.invoke.MethodHandles.Lookup;
 import static java.lang.invoke.MethodType.methodType;
 import javax.annotation.Nonnull;
 import nz.net.ultraq.thymeleaf.context.extensions.IContextExtensions;
@@ -11,23 +11,22 @@ import org.thymeleaf.dialect.IProcessorDialect;
 
 public class IContextDelegate {
 
-    private static final MethodHandle GET_PREFIX_FOR_DIALECT;
+    private static final MethodHandle GET_PREFIX_FOR_DIALECT = init();
 
-    static {
-        MethodHandle handle;
-        MethodHandles.Lookup lookup = MethodHandles.publicLookup();
+    @SuppressWarnings({"UseSpecificCatch", "BroadCatchBlock", "TooBroadCatch"})
+    private static MethodHandle init() {
+        Lookup lookup = lookup();
         try {
-            handle = filterReturnValue(insertArguments(lookup.findStatic(Class.forName("org.codehaus.groovy.runtime.InvokerHelper"), "invokeMethod", methodType(Object.class, Object.class, String.class, Object.class)).asCollector(Object[].class, 1),
-                    1, "getPrefixForDialect"),
-                    lookup.findStatic(Class.forName("org.codehaus.groovy.runtime.typehandling.ShortTypeHandling"), "castToString", methodType(String.class, Object.class)));
-        } catch (ReflectiveOperationException ex) {
             try {
-                handle = lookup.findStatic(IContextExtensions.class, "getPrefixForDialect", methodType(String.class, IContext.class, Class.class));
-            } catch (NoSuchMethodException | IllegalAccessException ex1) {
-                throw new AssertionError(ex1);
+                return filterReturnValue(insertArguments(lookup.findStatic(Class.forName("org.codehaus.groovy.runtime.InvokerHelper"), "invokeMethod", methodType(Object.class, Object.class, String.class, Object.class)).asCollector(Object[].class, 1),
+                        1, "getPrefixForDialect"),
+                        lookup.findStatic(Class.forName("org.codehaus.groovy.runtime.typehandling.ShortTypeHandling"), "castToString", methodType(String.class, Object.class)));
+            } catch (ReflectiveOperationException | LinkageError ignored) {
+                return lookup.findStatic(IContextExtensions.class, "getPrefixForDialect", methodType(String.class, IContext.class, Class.class));
             }
+        } catch (Throwable ex) {
+            return dropArguments(throwException(String.class, Throwable.class).bindTo(ex), 0, Object.class, Class.class);
         }
-        GET_PREFIX_FOR_DIALECT = handle;
     }
 
     public static String getPrefixForDialect(@Nonnull IContext self, Class<? extends IProcessorDialect> dialectClass) {
